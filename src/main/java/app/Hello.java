95dc12665665c9bfda2a689c450ed2a37a560d4a
Michal Kulinski <you@example.com>
2019-09-24 13:11:10 +0200

    Log4J implementation
    Logging support library implemented

--- src/main/java/app/Hello.java
+++ src/main/java/app/Hello.java
@@ -1,7 +1,14 @@
 package app;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Hello {
+    private static final Logger logger = LogManager.getLogger(Hello.class);
+
     public static void main(String[] args) {
+        logger.info("Start program");
+        
         System.out.println("Hello");
     }
 }
